d8123de44adf28af35dec8fc28697b78aa4be042
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2026-03-23 14:59:42 +0530

    changed health_check settings from '/' to '/health'

--- main.tf
+++ main.tf
@@ -402,7 +402,7 @@ resource "aws_lb_target_group" "app_tg" {
   deregistration_delay = 30
 
   health_check {
-    path                = "/"
+    path                = "/health"
     healthy_threshold   = 3
     unhealthy_threshold = 2
     timeout             = 5
